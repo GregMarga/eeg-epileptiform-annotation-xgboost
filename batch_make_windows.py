600c4b2b9a8bdebb41e7d529154087acdcde23ea
GregMarga <gregmargartis@gmail.com>
2026-03-01 21:44:57 +0100

    Preprocessing my EEG files for LaBraM foundation model

--- batch_make_windows.py
+++ batch_make_windows.py
@@ -9,53 +9,42 @@ def patient_id_from_filename(filename: str):
     m = re.match(r"^P(\d+)_", filename)
     return int(m.group(1)) if m else None
 
-
 def main():
-    data_dir = Path("../../data")
-    out_dir = data_dir / "windows_cache"
+    data_dir = Path("../../../data")
+    out_dir = data_dir / "labram_windows_cache"
     out_dir.mkdir(parents=True, exist_ok=True)
 
     edf_files = sorted(data_dir.glob("*.edf"))
 
-    # "before P28" => keep Pxx < 28
     selected = []
-    for p in edf_files:    ## ayto na to allaksw meta eksairei to arxeio 28
+    for p in edf_files:
         pid = patient_id_from_filename(p.name)
         if pid is None:
             continue
-        if pid != 28:
-            selected.append(p)
+        selected.append(p)
 
-    print(f"Found {len(selected)} EDF files with Pxx != 28")
+    print(f"Found {len(selected)} EDF files")
 
     for i, edf_path in enumerate(selected, start=1):
         print(f"[{i}/{len(selected)}] {edf_path.name}")
 
-        windows, labels, sfreq, ch_names, bad_chs = preprocessing.preprocess_edf_to_windows(
+        windows, ch_names = preprocessing.create_sliding_windows_from_eeg(
             edf_path=str(edf_path),
-            l_freq=0.5,
-            h_freq=40.0,
-            high_factor=8.0,
-            low_factor=10.0,
-            positive_label="*",
-            negative_label="-",
+            l_freq=0.1,
+            h_freq=75.0,
         )
 
         out_path = out_dir / f"{edf_path.stem}_windows.npz"
         np.savez_compressed(
             out_path,
             windows=windows,
-            labels=labels,
-            sfreq=sfreq,
             ch_names=ch_names,
-            bad_chs=bad_chs,
             edf_name=edf_path.name,
         )
 
-        print(f"  Saved: {out_path} | windows={windows.shape} positives={int(labels.sum())}/{len(labels)}")
+        print(f"  Saved: {out_path} | windows={windows.shape} ")
 
     print("Done.")
-
 
 if __name__ == "__main__":
     main()
--- preprocessing.py
+++ preprocessing.py
@@ -2,127 +2,52 @@ import mne
 import numpy as np
 from mne.channels import make_standard_montage
 from pathlib import Path
+from tqdm import tqdm
 
-# ---------------------------
-# 1. Detect bad channels
-# ---------------------------
-
-def detect_bad_channels_by_std(
-        raw: mne.io.BaseRaw,
-        high_factor: float = 8.0,
-        low_factor: float = 10.0,
-):
-    # find outlier (bad) channels based on std
-
-    data = raw.get_data()  # shape: (n_channels, n_samples)
-    stds = data.std(axis=1)  # std per channel
-    median_std = np.median(stds)
-
-    high_threshold = median_std * high_factor
-    low_threshold = median_std / low_factor
-
-    bad_high = np.where(stds > high_threshold)[0]  # too noisy eg electrode pop artifacts
-    bad_low = np.where(stds < low_threshold)[0]  # dead channel
-
-    bad_idx = np.unique(np.concatenate([bad_high, bad_low]))
-    bad_channels = [raw.ch_names[i] for i in bad_idx]
-
-    return bad_channels, stds
-
-
-def mark_and_interpolate_bad_channels(
-        raw: mne.io.BaseRaw,
-        high_factor: float = 8.0,
-        low_factor: float = 10.0,
-        plot: bool = False,
-):
-    bad_channels, stds = detect_bad_channels_by_std(
-        raw,
-        high_factor=high_factor,
-        low_factor=low_factor,
-    )
-
-    # print("STD per channel:")
-    # for name, s in zip(raw.ch_names, stds):
-    #     print(f"{name}: {s:.3e}")
-    # print("Detected bad channels:", bad_channels)
-
-    # Mark as bad
-    raw.info['bads'] = bad_channels
-
-    # Spatial interpolation
-    if len(bad_channels) > 0:
-        raw.interpolate_bads(reset_bads=True)
-        # print("Interpolated bad channels.")
-    # else:
-    #     print("No bad channels detected, nothing to interpolate.")
-
-    if plot:
-        raw.plot(scalings='auto', block=True)
-
-    return bad_channels
-
-
-
-def create_annotation_centered_epochs(
+def create_sliding_epochs(
         data,
-        annotations,
         sfreq,
-        positive_label='*',
-        negative_label='-',
-        window=250  # in ms
+        step_size=750,  # 75% of 1s -> 25% overlap
+        window=1000,    # 1 second
 ):
-    annotation_onsets = np.round(annotations.onset * sfreq).astype(int)
-    descriptions = np.array(annotations.description)
+    sample_window = int(round(window * sfreq / 1000))
+    last_start = data.shape[-1] - sample_window
+    step = int(round(step_size * sfreq / 1000))
 
-    labels = []
     epochs = []
+    for start in tqdm(range(0, last_start + 1, step),
+                      desc="Windows",
+                      leave=False):
+        epoch = data[:, start:start + sample_window]
+        epochs.append(epoch)
 
-    margin = int(sfreq * window / 1000)
-    n_samples = data.shape[1]
-    for i in range(len(annotations)):
-        epoch_center_idx = int(annotation_onsets[i])
-        start = epoch_center_idx - margin
-        stop = epoch_center_idx + margin
-
-        if start < 0 or stop > n_samples:
-            continue
-
-        if descriptions[i] == positive_label:
-            labels.append(1)
-            epoch = data[:, start:stop]
-            epochs.append(epoch)
-        elif descriptions[i] == negative_label:
-            labels.append(0)
-            epoch = data[:, start:stop]
-            epochs.append(epoch)
-
-    return np.array(epochs), np.array(labels)
+    return np.array(epochs)
 
 
-def preprocess_edf_to_windows(
+def create_sliding_windows_from_eeg(
         edf_path: str,
-        l_freq: float = 0.5,
-        h_freq: float = 40.0,
-        high_factor: float = 8.0,
-        low_factor: float = 10.0,
-        positive_label: str = '*',
-        negative_label: str = '-',
+        l_freq: float = 0.1,
+        h_freq: float = 75.0,
+        notch_freq: float = 50.0,
+        target_sfreq: float = 200.0,
 ):
+    data_dir = Path("../../../data")
+    out_dir = data_dir / "labram_windows_cache"
+    out_dir.mkdir(parents=True, exist_ok=True)
 
     raw = mne.io.read_raw_edf(edf_path, preload=True, verbose="ERROR")
 
-    # clean channel names
+    # 1) Keep only EEG channels (remove irrelevant channels)
+    raw.pick("eeg")
+
+    # Clean channel names
     raw.rename_channels(lambda ch: ch.replace('EEG ', '').strip())
 
-    # montage
+    # Set standard 10-20 montage
     montage = make_standard_montage("standard_1020")
     raw.set_montage(montage, match_case=False, on_missing='ignore')
 
-    # average reference
-    raw.set_eeg_reference('average', verbose="ERROR")
-
-    # filter
+    # 2) Bandpass 0.1–75 Hz
     raw.filter(
         l_freq=l_freq,
         h_freq=h_freq,
@@ -132,55 +57,24 @@ def preprocess_edf_to_windows(
         verbose="ERROR",
     )
 
-    # bad channels detection + interpolation (your existing function)
-    bad_chs = mark_and_interpolate_bad_channels(
-        raw,
-        high_factor=high_factor,
-        low_factor=low_factor,
-        plot=False,
-    )
+    # 3) Notch 50 Hz
+    raw.notch_filter(freqs=[notch_freq], verbose="ERROR")
 
-    data = raw.get_data()
-    sfreq = float(raw.info['sfreq'])
-    annotations=raw.annotations
+    # 4) Resample to 200 Hz
+    raw.resample(target_sfreq, npad="auto", verbose="ERROR")
 
-    epochs,labels=create_annotation_centered_epochs(data=data,sfreq=sfreq,annotations=annotations,positive_label=positive_label,negative_label=negative_label)
+    # 5) Convert from Volts to microvolts (μV)
+    data_uv = raw.get_data() * 1e6
 
+    sfreq = float(raw.info['sfreq'])  # should now be 200 Hz
     ch_names = np.array(raw.ch_names, dtype=object)
+
+    print("Data shape (channels, samples):", data_uv.shape)
+    print("Sampling frequency:", sfreq)
+
+    epochs = create_sliding_epochs(data=data_uv, sfreq=sfreq)
 
     return (
         epochs.astype(np.float32),
-        labels.astype(np.uint8),
-        sfreq,
         ch_names,
-        np.array(bad_chs, dtype=object),
     )
-
-## check
-# BASE = Path(__file__).resolve().parents[2]  # Thesis
-# edf_path = BASE / "Data" / "p28_GHB_00000_0002249_0001.edf"
-#
-# epochs, labels, sfreq, ch_names, bad_chs = preprocess_edf_to_windows(
-#     edf_path=str(edf_path),
-#     l_freq=0.5,
-#     h_freq=40.0,
-#     positive_label='*',
-#     negative_label='-',
-# )
-#
-# print("Epochs shape:", epochs.shape)   # (n_epochs, n_channels, n_samples)
-# print("Labels shape:", labels.shape)   # (n_epochs,)
-# print("Sampling freq:", sfreq)
-# print("Channels:", ch_names)
-# print("Bad channels:", bad_chs)
-#
-#
-# print("positives:", labels.sum())
-# print("negatives:", (labels == 0).sum())
-#
-# print("unique labels:", np.unique(labels))
-#
-# assert epochs.ndim == 3
-# assert len(epochs) == len(labels)
-# assert epochs.shape[1] == len(ch_names)
-
